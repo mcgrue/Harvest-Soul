d4ae9b9c615534ab14764d1432239479a8b8a28f
Ben McGraw <mcgrue@verge-rpg.com>
2011-03-30 17:22:46 -0700

    Refactoring making things draggable/droppable.  Started the shake-tracking system.

--- js/harvest_soul.js
+++ js/harvest_soul.js
@@ -10,17 +10,68 @@ $$.log = function(msg) {
     $('#logger').val( msg + "\n" + old );
 };
 
+$$.dragCounter = {
+    _ar : [];
+    push : function() {
+        var d = new Date();
+        _ar.push(d.getTime()); 
+    },
+}
+
 $$.Items = {
     makeDraggable : function(sel) {
-        $( sel ).draggable({ 
+        return $( sel ).draggable({ 
             snap: ".slot", 
             snapMode: "inner",
             revert: true,
             helper: "clone",
             opacity: 0.7,
-            stack: ".item" 
+            stack: ".item", 
+
+            drag: function() {
+                counts[1]++;
+                updateCounterStatus( $drag_counter, counts[ 1 ] );
+            }
         });
     }
 }
 
+/*
+*/
 
+$$.Slots = {
+    makeDroppable : function(sel) {
+        return $(sel).droppable({
+            drop: function( event, ui ) {
+                var slot = $(event.target);
+                var kids = slot.children();
+
+                if( kids.length ) {
+                    var socketed = $(kids[0]);
+                    var newItem = $$.canMix(socketed, ui.draggable);
+                    if( newItem ) {
+                        socketed.remove();
+                        $( ui.draggable ).draggable( "option", "revert", false );
+                        ui.draggable.remove();
+                        var node = $(slot).append('<div class="item '+newItem+'"></div>');
+                        $$.Items.makeDraggable( $(node.children()[0]) );
+                        return;
+                    } else {
+                        return false;
+                    }
+                }
+
+                $( ui.draggable ).draggable( "option", "revert", false );
+                var node = ui.draggable.detach();
+
+                node.css( {'left':0, 'top':0} );
+                node.appendTo(slot);
+
+                $$.log( 'Item dropped into slot.' );
+                setTimeout( function() {
+                    $( ui.draggable ).draggable( "option", "revert", true );
+                }, 10);
+            }
+        });
+    }
+}
